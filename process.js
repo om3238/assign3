a9f2014f050a7480426cf1d7c6f7b930a33fe95b
om3238 <119293581+om3238@users.noreply.github.com>
2023-12-25 15:36:50 +0530

    Add files via upload

---
+++ process.js
@@ -0,0 +1,17 @@
+function validateLogin() {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    const usernameRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const isUsernameValid = usernameRegex.test(username);
+
+    const passwordRegex = /^(?=.*[A-Z])(?=.*\d)[A-Za-z\d@]+$/;
+    const isPasswordValid = passwordRegex.test(password);
+
+    document.getElementById('usernameError').textContent = isUsernameValid ? '' : 'Enter a valid username';
+    document.getElementById('passwordError').textContent = isPasswordValid ? '' : 'Password must contain an uppercase letter, a number, and only @ as a special character';
+
+    if (isUsernameValid && isPasswordValid) {
+        alert('Login successful!');
+    }
+}
